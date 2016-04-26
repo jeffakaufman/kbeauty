d52e39b2cca0702ace03370f08eac0e8a536360f
aleksey <azagarov@mail.ru>
2016-04-26 11:59:45 +0600

    Testing Dates !!! TODO :: Remove After Testing

--- app/code/local/Valkyrie/SliderData/controllers/Adminhtml/SliderdataController.php
+++ app/code/local/Valkyrie/SliderData/controllers/Adminhtml/SliderdataController.php
@@ -14,6 +14,10 @@ class Valkyrie_SliderData_Adminhtml_SliderDataController extends Mage_Adminhtml_
       $contentBlock = $this->getLayout()->createBlock('sliderdata/adminhtml_sliderdata');
       $this->_addContent($contentBlock);
 
+      $now = new DateTime('now');
+var_dump($now->getTimezone());
+var_dump($now);
+
     $this->renderLayout();
   }
 
