3d96fc427d287d375bd8e66a900b71f9280cd3c4
agedgouda <jeffkaufman@kaufmaninternational.com>
2016-06-29 10:58:45 -0700

    boost

--- app/code/local/Boost/Pay/Model/Pay.php
+++ app/code/local/Boost/Pay/Model/Pay.php
@@ -6,5 +6,12 @@ class Boost_Pay_Model_Pay extends Mage_Payment_Model_Method_Abstract
   protected $_canUseCheckout = false;
   protected $_canCapture = true;
 
+  public function capture(Varien_Object $payment, $amount)
+  {
+    $payment->setTransactionId($payment->getAdditionalInformation('boost.transactionId'));
+    $payment->setIsTransactionClosed(1);
+    $payment->save();
+    return $this;
+  }
 }
 ?>
