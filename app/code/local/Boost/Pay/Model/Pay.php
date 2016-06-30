c58cd30a7132023f690ffbcfd324288a7ba6da93
Masahji Stewart <masahji@boo.st>
2016-06-30 06:10:48 -0700

    Adding support for revealing Boost transaction details in admin order page

--- app/code/local/Boost/Pay/Model/Pay.php
+++ app/code/local/Boost/Pay/Model/Pay.php
@@ -5,6 +5,7 @@ class Boost_Pay_Model_Pay extends Mage_Payment_Model_Method_Abstract
   protected $_canUseInternal = true;
   protected $_canUseCheckout = false;
   protected $_canCapture = true;
+  protected $_infoBlockType = 'boost/info_boost';
 
   public function capture(Varien_Object $payment, $amount)
   {
